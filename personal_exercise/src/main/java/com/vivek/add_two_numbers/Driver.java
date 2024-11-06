02e4f95750d4ce336d3cfde65880d64cf97fe996
vivek_pachauri <pach.vivek@gmail.com>
2024-11-06 18:32:39 -0500

    added node class

--- personal_exercise/src/main/java/com/vivek/add_two_numbers/Driver.java
+++ personal_exercise/src/main/java/com/vivek/add_two_numbers/Driver.java
@@ -2,4 +2,10 @@ package com.vivek.add_two_numbers;
 
 public class Driver {
 
+    public static void main(String[] args) {
+        ListNode node = new ListNode(10);
+        System.out.println(node);
+        return;
+    }
+
 }
