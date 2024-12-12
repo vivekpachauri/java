698bbb092acf5f86d8ccd248adbd93d74fdbfc81
vivek_pachauri <pach.vivek@gmail.com>
2024-12-12 16:04:45 -0500

    added solution for merge sorted lists

--- personal_exercise/src/main/java/com/vivek/add_two_numbers/ListNode.java
+++ personal_exercise/src/main/java/com/vivek/add_two_numbers/ListNode.java
@@ -3,11 +3,49 @@ package com.vivek.add_two_numbers;
 public class ListNode {
      int val;
      ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     public ListNode() {}
+     public ListNode(int val) { this.val = val; }
+     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     @Override
         public String toString() {
             return "ListNode [val=" + val + ", next=" + next + "]";
         }
+    public int getVal() {
+        return val;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + val;
+        result = prime * result + ((next == null) ? 0 : next.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ListNode other = (ListNode) obj;
+        if (val != other.val)
+            return false;
+        if (next == null) {
+            if (other.next != null)
+                return false;
+        } else if (!next.equals(other.next))
+            return false;
+        return true;
+    }
+    public void setVal(int val) {
+        this.val = val;
+    }
+    public ListNode getNext() {
+        return next;
+    }
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
     }
---
+++ personal_exercise/src/main/java/com/vivek/merge_sorted_lists/Driver.java
@@ -0,0 +1,79 @@
+package com.vivek.merge_sorted_lists;
+
+import com.vivek.add_two_numbers.ListNode;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        ListNode listNodeOne = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode listNodeTwo = new ListNode(4, new ListNode(5, new ListNode(6, null)));
+        System.out.println(driver.mergeTwoLists(listNodeOne, listNodeTwo));
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        /*
+         * starting - any of the lists could be the beginning, check the two heads and pick the smaller one
+         */
+        ListNode start;
+        if (list1.getVal() < list2.getVal()) {
+            start = list1;
+            mergeListRecursive(list1.getNext(), list2, start);
+        }
+        else {
+            start = list2;
+            mergeListRecursive(list1, list2.getNext(), start);
+        }
+        System.out.println("final result " + start);
+
+        
+
+        /*
+         * next check which of the element is smaller
+         */
+        return start;
+    }
+
+    private void mergeTwoListsIterative(ListNode list1, ListNode list2, ListNode result) {
+
+        while (list1 != null && list2 != null ) {
+
+        }
+
+    }
+
+    private void mergeListRecursive(ListNode list1, ListNode list2, ListNode result) {
+
+        //base case
+        //if both the lists are empty then return the result
+
+        if (list1 == null && list2 == null ) {
+            return;
+        }
+
+        //if first is empty then simply tack on the second one and return the result
+        else if (list1 == null ) {
+            result.setNext(list2);
+            return;
+        }
+
+        //if second is empty then simply tack on the first one and return the result
+        else if (list2 == null ) {
+            result.setNext(list1);
+            return;
+        }
+        else {
+            //pick the next node from the two lists and make the recursive call
+            if (list1.getVal() < list2.getVal()) {
+                 result.setNext(list1);
+                 mergeListRecursive(list1.getNext(), list2, result.getNext());
+            }
+            else {
+                result.setNext(list2);
+                mergeListRecursive(list1, list2.getNext(), result.getNext());
+            }
+        }
+
+
+
+    }
+}
