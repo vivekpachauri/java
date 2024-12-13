644650dd7df28a15c738001fde5356a1197453ed
vivek_pachauri <pach.vivek@gmail.com>
2024-12-13 16:42:04 -0500

    rotate with copy improved

--- personal_exercise/src/main/java/com/vivek/rotate_array/Driver.java
+++ personal_exercise/src/main/java/com/vivek/rotate_array/Driver.java
@@ -6,23 +6,20 @@ import java.util.function.IntFunction;
 public class Driver {
 
     public void rotate(int[] nums, int k) {
+        // if ( nums.length < k ) {
+        //     rotateInPlace(nums, k);
+        // }
+        // else {
+        //     rotateWithCopy(nums, k);
+        // }
         rotateWithCopy(nums, k);
-
-        //arrayTest(nums, k);
-
-
     }
-
-    // private void arrayTest(int[] nums, int k) {
-    //     Arrays.setAll(nums, i -> nums[i]+1);
-    //     for (int i : nums) {
-    //         System.out.print(i + " ,");
-    //     }
-    // }
 
     private void rotateInPlace(int[] nums, int k) {
         /*
-        [1,2,3,4,5]
+        [1,2,3,4,5]       [1,2,3,4,5]    [1,2,3,4,5]
+        [1,2,3,4,5], 3 -> [-,-,1,2,3] -> [4,-,1,2,3]
+        [1,2], 3
          * iterative
          * keep swapping by 2,
          * will have to make the entire array rotate by k number
@@ -41,7 +38,8 @@ public class Driver {
     }
 
     private void rotateWithCopy(int[] nums, int k) {
-        if ( nums.length == 1 ) return;
+        k = k % nums.length;
+        //if ( nums.length == 1 ) return;
         //let's try with creating a new copy
         //create an array of the same size
         int[] newCopy = Arrays.copyOf(nums, nums.length);//new int[nums.length];
@@ -57,17 +55,25 @@ public class Driver {
             nums[i+k] = newCopy[i];
         }
         //now take the k+1 onwards indexess and insert them in the new array
-        for ( int i = nums.length - k, j = 0; i < nums.length; i++, j++ ) {
-            nums[j] = newCopy[i];
-        }
+//        if ( nums.length > k) {
+            
+            for ( int i = nums.length - k, j = 0; i < nums.length; i++, j++ ) {
+                nums[j] = newCopy[i];
+            }
+        // }
+        // else {
+        //     for ( int i = 0; i < k; i++ ) {
+        //         swap
+        //     }
+        // }
         //nums=newCopy;
         
     }
 
     public static void main(String[] args) {
         Driver driver = new Driver();
-        int[] input = new int[] {1};
-        driver.rotate(input, 2);
+        int[] input = new int[] {1,2};
+        driver.rotate(input, 3);
         for (int i : input) { System.out.print(i + ",");}
     }
 
