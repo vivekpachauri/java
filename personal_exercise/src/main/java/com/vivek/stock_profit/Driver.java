ec32410065b5393444a1b9cafa5c7f94e4fbbfb5
vivek_pachauri <pach.vivek@gmail.com>
2024-12-18 15:10:33 -0500

    potential solution to stock option problem

--- personal_exercise/src/main/java/com/vivek/stock_profit/Driver.java
+++ personal_exercise/src/main/java/com/vivek/stock_profit/Driver.java
@@ -60,6 +60,9 @@ public class Driver {
       */
 
     public int maxProfit(int[] prices) {
+        if (prices.length == 1 ) { 
+            return 0;
+        }
         int[][] priceDifference = new int[prices.length][prices.length];
         for ( int i = 0; i < prices.length; i++ ) {
             Arrays.fill(priceDifference[i], -1);
@@ -73,37 +76,71 @@ public class Driver {
         }
         printArray(priceDifference);
 
-        int max = 0;
-        // for ( int i = 0; i < priceDifference.length; i++ ) {
-        //     for ( int j = 0; j < priceDifference[i].length; j++ ) {
+        // int max = 0;
+        // // for ( int i = 0; i < priceDifference.length; i++ ) {
+        // //     for ( int j = 0; j < priceDifference[i].length; j++ ) {
                 
-        //     }
-        // }
-        int purchaseDay = 0, profitDay = 0, maxProfitPurchaseDay = 0;
+        // //     }
+        // // }
+        // int purchaseDay = 0, profitDay = 0, maxProfitPurchaseDay = 0;
 
-        while ( purchaseDay < priceDifference.length) {
-            while ( profitDay < priceDifference[purchaseDay].length ) {
-                if ( max < priceDifference[purchaseDay][profitDay] ) {
-                    max = priceDifference[purchaseDay][profitDay];
-                    maxProfitPurchaseDay = purchaseDay;
-                }
+        // while ( purchaseDay < priceDifference.length) {
+        //     while ( profitDay < priceDifference[purchaseDay].length ) {
+        //         if ( max < priceDifference[purchaseDay][profitDay] ) {
+        //             max = priceDifference[purchaseDay][profitDay];
+        //             maxProfitPurchaseDay = purchaseDay;
+        //         }
+        //     }
+
+        // }
+        return calculateMaxProfit(prices, 0, 0);
+    }
+
+    private int calculateMaxProfit(int[] prices, int currentProfit, int purchasePrice) {
+//1,5
+//5,1
+//1,6,5
+//5,1,6
+        // base case of 2 element
+        // to check if we should purchase today or not,
+        // if second is greater than 1 then we should purchase, if not then we should keep the current profit and track the purchase price
+        // if 1 element then
+        if (prices.length == 1 ) {
+            if ( purchasePrice == 0 ) {
+                return currentProfit;
+            }
+            else {
+                int difference = prices[0] - purchasePrice;
+                return Math.max(currentProfit + difference, currentProfit);
             }
 
         }
-        return 0;
-    }
-
-    private int calculateMaxProfit(int[][] priceDifference, int buyDay) {
-        //check for each day how much money could be made if the stock is sold from the beginning until that day
-
-        //total number of days are n
-        //therefore is stock is sold on the nth day and purchased on the first day then nothing more is left
-        //if the stock is sold on n-1st day and then purchased back then how much money could be made on nth day
-        for ( int i = 0, j = 0; i < priceDifference.length; i++, j++ ) {
-
+        else {
+            if ( prices[1] > prices[0] ) {
+                int profitIncrease = prices[1] - prices[0];
+                return calculateMaxProfit(Arrays.copyOfRange(prices, 1, prices.length), currentProfit + profitIncrease, prices[1]);
+            }
+            else {
+                return calculateMaxProfit(Arrays.copyOfRange(prices, 1, prices.length), currentProfit , purchasePrice);
+            }
         }
-        return 0;
     }
+
+    // private int calculateMaxProfit(int[][] priceDifference, int buyDay) {
+    //     //check for each day how much money could be made if the stock is sold from the beginning until that day
+    //     // 7,2,1,6,5,8
+    //     //total number of days are n
+    //     //therefore is stock is sold on the nth day and purchased on the first day then nothing more is left
+    //     //if the stock is sold on n-1st day and then purchased back then how much money could be made on nth day
+    //     int maxEarning = 0, maxEarningPurchaseDay = 0;
+    //     for ( int i = 0, j = 0; i < priceDifference.length; i++, j++ ) {
+    //         if ( maxEarning > priceDifference[i][j] ) {
+    //             maxEarning = priceDifference[i][j];
+    //             maxEarningPurchaseDay = j;
+    //         }
+    //     }
+    //     return 0;
+    // }
 
     private void printArray(int[][] toPrint) {
         for ( int i = 0; i < toPrint.length; i++ ) {
@@ -115,8 +152,19 @@ public class Driver {
     }
 
     public static void main(String[] args) {
+        //1,5
+//5,1
+//1,6,5
+//5,1,6
         Driver driver = new Driver();
-        int[] prices = new int[] { 7,2,1,6,5,8 };
+        int[] prices = 
+        //new int[] {1, 6, 5};
+        new int[] { 7,2,1,6,5,8 };
+        System.out.print("[ ");
+        for ( int i : prices ) {
+            System.out.print(i + ",");
+        }
+        System.out.println(" ]");
         System.out.println("total profit = " + driver.maxProfit(prices));
     }
 }
