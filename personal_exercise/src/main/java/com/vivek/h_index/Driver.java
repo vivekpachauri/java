9318b871785a5f3073269490f0d5c3470a7f7e47
vivek_pachauri <pach.vivek@gmail.com>
2024-12-19 19:22:32 -0500

    setup for h-index problem

---
+++ personal_exercise/src/main/java/com/vivek/h_index/Driver.java
@@ -0,0 +1,13 @@
+package com.vivek.h_index;
+
+public class Driver {
+    public int hIndex(int[] citations) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        int[] citations = {3, 0, 6, 1, 5};
+        System.out.println(driver.hIndex(citations));
+    }
+}
