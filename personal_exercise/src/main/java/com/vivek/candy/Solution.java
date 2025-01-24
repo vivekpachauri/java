28b8038f0f326edcfbf953a9ea5b70a398ceea3d
vivek_pachauri <pach.vivek@gmail.com>
2025-01-23 19:03:04 -0500

    candy problem solution working for large test cases

--- personal_exercise/src/main/java/com/vivek/candy/Solution.java
+++ personal_exercise/src/main/java/com/vivek/candy/Solution.java
@@ -56,15 +56,26 @@ class Solution {
             candies[i] = 1;
         }
 
-        while (!isValid(candies, ratings)) {
-            updateCandies(candies, ratings);
-        }
+        boolean updated;// = true;
+        do {
+            updated = updateCandies(candies, ratings);
+        } while (updated);
+        // while (!isValid(candies, ratings)) {
+        //     updateCandies(candies, ratings);
+        // }
 
         return Arrays.stream(candies).sum();
 
     }
 
-    private void updateCandies(int[] candies, int[] ratings) {
+    // private void updateCandiesOnce(int[] candies, int[] ratings) {
+    //     for ( int i = 0; i < ratings.length; i++ ) {
+
+    //     }
+    // }
+
+    private boolean updateCandies(int[] candies, int[] ratings) {
+        boolean updated = false;
         for (int i = 0; i < ratings.length; i++) {
             // if i has a higher rating but less candies than it's left or right neighbor
             // then increas the number of cadies by the number of candies of the neighbor
@@ -72,6 +83,7 @@ class Solution {
             if (i == 0) {
                 if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
                     candies[i] += 1;// candies[i+1];
+                    updated = true;
                 }
             }
 
@@ -79,6 +91,7 @@ class Solution {
             else if (i == (ratings.length - 1)) {
                 if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
                     candies[i] += 1;// candies[i-1];
+                    updated = true;
                 }
             }
 
@@ -86,12 +99,15 @@ class Solution {
             else {
                 if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
                     candies[i] += 1;// candies[i-1];
+                    updated = true;
                 }
                 if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
                     candies[i] += 1;// candies[i+1];
+                    updated = true;
                 }
             }
         }
+        return updated;
     }
 
     private boolean isValid(int[] candies, int[] ratings) {
@@ -123,7 +139,7 @@ class Solution {
             else if (i == (ratings.length - 1)) {
                 if (ratings[i - 1] > ratings[i]) {
                     if (candies[i - 1] > candies[i]) {
-                        System.out.println("all good for index " + i + " and " + (i + 1));
+                        System.out.println("all good for index " + (i - 1) + " and " + i);
                     } else {
                         System.out.println("invalid -- rating of " + i + ": " + ratings[i]
                                 + " is greater than rating of " + (i + 1) + ": " + ratings[i + 1]);
@@ -143,7 +159,7 @@ class Solution {
                 // check left neighbor
                 if (ratings[i - 1] > ratings[i]) {
                     if (candies[i - 1] > candies[i]) {
-                        System.out.println("all good for index " + i + " and " + (i + 1));
+                        System.out.println("all good for index " + (i-1) + " and " + i);
                     } else {
                         System.out
                                 .println("invalid -- rating of " + i + ": " + ratings[i] + " is greater than rating of "
@@ -152,7 +168,7 @@ class Solution {
                     }
                 } else if (ratings[i - 1] < ratings[i]) {
                     if (candies[i - 1] < candies[i]) {
-                        System.out.println("all good for index " + i + " and " + (i + 1));
+                        System.out.println("all good for index " + (i-i) + " and " + i);
                     } else {
                         System.out.println("invalid at index " + i);
                         return false;
@@ -267,14 +283,42 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] ratings1 = { 1, 0, 2 };
-        int[] ratings2 = { 1, 2, 2 };
-        int[] ratings3 = { 1, 4, 3, 2 };
-        int[] ratings4 = { 1,2,87,87,87,2,1};
+        solution.testRatings();
+    }
 
-        System.out.println("####Minimum candies needed for ratings1: " + solution.candy(ratings1)); // Output: 5
-        System.out.println("####Minimum candies needed for ratings2: " + solution.candy(ratings2)); // Output: 4
-        System.out.println("####Minimum candies needed for ratings3: " + solution.candy(ratings3)); // Output: 7
-        System.out.println("####Minimum candies needed for ratings3: " + solution.candy(ratings4)); // Output: 7
+    private void testRatings() {
+        testRatings1();
+        testRatings2();
+        testRatings3();
+        testRatings4();
+        testRatings5();
+    }
+
+    private void testRatings1() {
+        int[] ratings1 = { 1, 0, 2 };
+        System.out.println("####Minimum candies needed for ratings1: " + candy(ratings1)); // Output: 5
+    }
+
+    private void testRatings2() {
+        int[] ratings2 = { 1, 2, 2 };
+        System.out.println("####Minimum candies needed for ratings2: " + candy(ratings2)); // Output: 4
+    }
+
+    private void testRatings3() {
+        int[] ratings3 = { 1, 4, 3, 2 };
+        System.out.println("####Minimum candies needed for ratings3: " + candy(ratings3)); // Output: 7
+    }
+
+    private void testRatings4() {
+        int[] ratings4 = { 1, 2, 87, 88, 87, 2, 1 };
+        System.out.println("####Minimum candies needed for ratings3: " + candy(ratings4)); // Output: 13
+    }
+
+    private void testRatings5() {
+        int[] ratings5 = new int[20001];
+        for ( int i = 1; i <= 20000; i++ ) {
+            ratings5[i] = i;
+        }
+        System.out.println("####Minimum candies needed for ratings3: " + candy(ratings5)); // Output: 7
     }
 }
