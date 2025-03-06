b8a619bda28c06648c7a518d65a14079d2a15d6e
vivek_pachauri <pach.vivek@gmail.com>
2025-03-06 11:38:37 -0500

    formatting

--- personal_exercise/src/main/java/com/vivek/happy_number/Solution.java
+++ personal_exercise/src/main/java/com/vivek/happy_number/Solution.java
@@ -6,41 +6,41 @@ import java.util.Set;
 class Solution {
     public boolean isHappy(int n) {
         Set<Integer> numbersSeen = new HashSet<>();
-        
+
         int[] tokenized = tokenize(n);
         int sum = 0;
         boolean seenAlready = false;
         do {
             sum = 0;
-            for ( int i = 0; i < tokenized.length; i++ ) {
+            for (int i = 0; i < tokenized.length; i++) {
                 sum += tokenized[i];
             }
             seenAlready = numbersSeen.contains(sum);
-            if ( seenAlready ) {
+            if (seenAlready) {
                 return false;
             }
             numbersSeen.add(sum);
             tokenized = tokenize(sum);
-        } while ( (sum != 1) );
+        } while ((sum != 1));
         return true;
-
     }
 
     private int[] tokenize(int n) {
         char[] charArray = String.valueOf(n).toCharArray();
         int[] intArray = new int[charArray.length];
-        for ( int i = 0; i < charArray.length; i++ ) {
+        for (int i = 0; i < charArray.length; i++) {
             intArray[i] = Integer.parseInt(String.valueOf(charArray[i]));
         }
         return intArray;
     }
-public static void main(String[] args) {
-    Solution solution = new Solution();
-    
-    // Test cases
-    int[] testCases = {19, 2, 7, 28, 100};
-    for (int testCase : testCases) {
-        System.out.println("Is " + testCase + " a happy number? " + solution.isHappy(testCase));
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test cases
+        int[] testCases = { 19, 2, 7, 28, 100 };
+        for (int testCase : testCases) {
+            System.out.println("Is " + testCase + " a happy number? " + solution.isHappy(testCase));
+        }
     }
-}
 }
