b3672fc858da4d5b12012ec03c0e72c3fc9f4f54
vivek_pachauri <pach.vivek@gmail.com>
2025-03-06 11:39:22 -0500

    added solution for finding missing positive integer

--- personal_exercise/src/main/java/com/vivek/first_missing_positive/Solution.java
+++ personal_exercise/src/main/java/com/vivek/first_missing_positive/Solution.java
@@ -1,5 +1,7 @@
 package com.vivek.first_missing_positive;
 
+import java.util.Arrays;
+
 public class Solution {
 
     /*
@@ -34,24 +36,74 @@ public class Solution {
      * -231 <= nums[i] <= 231 - 1
      */
 
-     public int firstMissingPositive(int[] nums) {
-        int smallestPosible = 1;
-        int largetstPossible = 1;
-        for ( int i = 0; i < nums.length; i++ ) {
-            if ( nums[i] < 1) {
-                //doesn't tell us anything
-            }
-            //if we find the smallest value possible in the array then increment the smallest possible
-            else if (nums[i] == smallestPosible ) {
-                smallestPosible++;
+    public int firstMissingPositive(int[] nums) {
+        // int smallestPosible = 1;
+        // int largetstPossible = 1;
+        // //re-order around pivot of 0
+        // int lastNegativeIndex = 0;
+        // for ( int i = 0; i < nums.length; i++ ) {
+        //     if ( nums[i] < 0) {
+        //         //doesn't tell us anything
+        //         swap(lastNegativeIndex, i, nums);
+        //         lastNegativeIndex++;
+        //     }
+        // }
 
-                //largest possible must be equal to or greater than smallest possible
-                if ( largetstPossible < smallestPosible ) {
-                    largetstPossible = smallestPosible;
-                }
+        // printArray(nums);
+
+        // int index = lastNegativeIndex;
+        // //cyclic sort from lastNegativeIndex onwards
+        // while ( index < nums.length ) {
+        //     int expectedValue = (1 + (index % lastNegativeIndex));
+        //     System.out.println("index " + index + " expectedValue " + expectedValue);
+        //     int correct = nums[index] + lastNegativeIndex;
+        //     if ( (nums[index] < (nums.length - 1)) && (nums[index] != expectedValue) ) {
+        //         swa
+        //     }
+
+        // }
+        // return 0;
+        /*
+         * think out loud
+         * we have list of integers, unsorted
+         * sorting is probably not an option being n squared
+         * we can find the min and max in o(1)
+         * we can find the diff between current num and the min or max
+         * what we want to find are the chains
+         */
+
+        int index = 0;
+        while ( index < nums.length ) {
+            int correct = nums[index] - 1;
+            if ( ( nums[index] > 0 ) && (nums[index] != nums[correct]) ) {
+                swap(index, correct, nums);
+            }
+            else {
+                index++;
             }
         }
-        //TODO finish implementation
+        printArray(nums);
+        for ( int i = 0; i < nums.length; i++ ) {
+            if ( nums[i] != (i + 1) ) {
+                return (i + 1);
+            }
+        }
         return 0;
+     }
+
+     private void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+     }
+
+     private void printArray(int[] arr) {
+        Arrays.stream(arr).forEach(a -> System.out.print(a + " -> "));
+     }
+
+     public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = new int[]{-1,0,-2,4,5,0,-3, 1};
+        System.out.println("missing number is " + s.firstMissingPositive(arr));
      }
 }
