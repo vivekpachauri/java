d0feaf35d054df8f6075600ea0fd0c8a08dbbcc7
vivek_pachauri <pach.vivek@gmail.com>
2025-03-06 17:22:43 -0500

    added exercise for missing and dupliacate using cyclic sort

--- personal_exercise/src/main/java/com/vivek/cyclic_duplicate_number/Solution.java
+++ personal_exercise/src/main/java/com/vivek/cyclic_duplicate_number/Solution.java
@@ -1,6 +1,41 @@
 package com.vivek.cyclic_duplicate_number;
 
 class Solution {
+
+    public int[] findMissingAndDuplicate(int[] nums) {
+        //return array containing number which is missing and which is duplicate
+        int[] result = new int[2];
+        int index = 0;
+        while ( index < nums.length ) {
+            int correctPosition = nums[index] - 1;
+            //0 index should contain 1, 1 index should contain 2 etc
+            if ( nums[index] != nums[correctPosition] ) {
+                int temp = nums[correctPosition];
+                nums[correctPosition] = nums[index];
+                nums[index] = temp;
+            }
+            else {
+                index++;
+            }
+        }
+
+        //scan and find the out of place value and it's index
+        for ( int i = 0; i < nums.length; i++ ) {
+            if ( nums[i] != (i+1) ) {
+                result = new int[] {(i+1), nums[i]};
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = new int[] {5,6,6,1,2,3,4};
+        for ( int i : s.findMissingAndDuplicate(arr) ) {
+            System.out.print(i + " -> " );
+        }
+    }
+
     public int findDuplicate(int[] nums) {
         int result = 0;
         int index = 0;
